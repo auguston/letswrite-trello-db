e3db5d7bbe5985b9c4ec295c5a53aa7d843ff9ce
auguston <evilkim25@gmail.com>
2019-10-15 20:52:23 +0800

    頁面完成

--- src/main.js
+++ src/main.js
@@ -1,50 +1,46 @@
 const ShopList = new Vue({
   el: '#app',
   data: {
+    mrt: '公館', // 站名
     cardName: '', // 卡片名稱
     shops: {}, // 店家資料
     shop: {} // 詳細資訊是哪個店家
   },
-  created() {
-
+  methods: {
     // 取得店家資料
-    function getShops(url) {
-      return new Promise((resolve, reject) => {
+    getShops(mrt) {
+      let url;
+      if(mrt === '公館') {
+        this.mrt = '公館';
+        url = 'https://trello.com/card/5da2c8a3f11397244be7ad22/-.json'
+      }
+      else if(mrt === '古亭') {
+        this.mrt = '古亭';
+        url = 'https://trello.com/card/5da5b564ca7ce5220c35c047/-.json'
+      };
 
-        fetch(url)
-          .then(res => res.json())
-          .then(results => {
-            
-            // 存卡片名稱
-            const cardName = results.name;
+      fetch(url)
+        .then(res => res.json())
+        .then(results => {
+          
+          // 存卡片名稱
+          this.cardName = results.name;
 
-            // 整理店家資料
-            let shops = {};
-            Array.prototype.forEach.call(results.checklists, (list, i) => {
-              shops[list.id] = Object.create(null, {
-                id: { value: list.id },
-                name: { value: list.name },
-                site: { value: list.checkItems[0].name },
-                tel: { value: list.checkItems[1].name }
-              });
+          // 整理店家資料
+          this.shops = {};
+          Array.prototype.forEach.call(results.checklists, (list, i) => {
+            this.shops[list.id] = Object.create(null, {
+              id: { value: list.id },
+              name: { value: list.name },
+              site: { value: list.checkItems[0].name },
+              tel: { value: list.checkItems[1].name }
             });
+          });
 
-            resolve([cardName, shops]);
-
-          })
-          .catch(err => reject(err));
-
-      });
+        })
     }
-
-    getShops('https://trello.com/card/5da2c8a3f11397244be7ad22/-.json')
-      .then(result => {
-        this.cardName = result[0];
-        this.shops = result[1];
-        console.log(this.shops)
-      });
-
-    
-
+  },
+  created() {
+    this.getShops('公館');
   },
 })
